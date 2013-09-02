ea4bd0dd624f34374fb32fbcab17c653145cab80
Federico Cargnelutti <fedecarg@gmail.com>
2013-09-02 16:58:26 +0100

    Added the option to pass the project name as an arg

--- sublp.sh
+++ sublp.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-COUNT=$(ls *.sublime-project 2>/dev/null | wc -l)
+if [ "${1}" ] && [ -f "${1}.sublime-project" ]; then
+    subl --project "${1}.sublime-project"
+    exit 1
+fi
 
+COUNT=$(ls *.sublime-project 2>/dev/null | wc -l)
 if [ $COUNT == "0" ]; then
-    subl .
+    echo "Unable to find a valid .sublime-project file"
 elif [ $COUNT == "1" ]; then
     FILENAME=$(ls *.sublime-project 2>/dev/null | head -n 1)
     subl --project $FILENAME
-else    
+else
     echo "Enter the number of the project you want to open:"
     select FILENAME in *.sublime-project; do
         if [ -f "$FILENAME" ]; then 
