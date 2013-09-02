d27e704b62324cf31c16ee6058907e1993035111
Federico Cargnelutti <fedecarg@gmail.com>
2013-09-02 17:03:24 +0100

    Improved ux ;)

--- sublp.sh
+++ sublp.sh
@@ -12,7 +12,7 @@ elif [ $COUNT == "1" ]; then
     FILENAME=$(ls *.sublime-project 2>/dev/null | head -n 1)
     subl --project $FILENAME
 else
-    echo "Enter the number of the project you want to open:"
+    echo "Enter the number of the project you want to open (or q to quit):"
     select FILENAME in *.sublime-project; do
         if [ -f "$FILENAME" ]; then 
             subl --project $FILENAME
